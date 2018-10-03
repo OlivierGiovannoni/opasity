1ba9dbcb00d0dd60a6bba0fe5a6fd0d4312a4114
Skerp <mohammed.brahmi@epitech.eu>
2018-10-03 17:20:11 +0200

    Added HTML Login Form => PHP Script that adds to the tests DB, soon deleting everything NodeJS related, unpaid.js package*.json and node_modules/

---
+++ web/login.php
@@ -0,0 +1,32 @@
+<?php
+
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+
+if (!empty($username) && !empty($password)) {
+
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "stage972";
+    //$dbname = "OPAS";
+    $dbname = "tests";
+
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+    if (mysqli_connect_error()){
+        die('Connect Error ('. mysqli_connect_errno() .') ' . mysqli_connect_error());
+    } else {
+        $sql = "INSERT INTO users (username, password) values ('$username','$password')";
+        if ($conn->query($sql)) {
+            echo "New record is inserted sucessfully";
+        } else {
+            echo "Error: ". $sql ." // ". $conn->error;
+        }
+        $conn->close();
+    }
+} else {
+    echo "Username or password should not be empty";
+    die();
+}
+
+?>
