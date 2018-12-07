724aeaeee65941b2b705e34ff071ef28b0748865
Skerp <mohammed.brahmi@epitech.eu>
2018-12-07 17:48:04 +0100

    qzmoefjid

--- php/searchClientOrders.php
+++ php/searchClientOrders.php
@@ -74,6 +74,7 @@ function selectLastComment($orderId, $orderIdShort, $paidStr)
 
         $rowComment = mysqli_fetch_array($resultComment);
         $paidCompta = isItPaid($rowComment['Commande'], "webcontrat_contrat", "connectionR");
+        echo "ZEIOJFP " . $rowComment['Reglement'] .  " qzpeofjp";
         if ($rowComment['Reglement'] != "R")
             echo "<td id=\"isPaid\">No</td>";
         else if ($paidCompta != "R")
